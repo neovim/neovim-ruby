41615d781dbd62cac4f8ef3217fd14e335e58f3d
Alex Genco <alexgenco@gmail.com>
2014-05-21 20:11:25 -0700

    rpc can raise exceptions

--- lib/neovim/rpc.rb
+++ lib/neovim/rpc.rb
@@ -2,12 +2,22 @@ require "msgpack"
 
 module Neovim
   class RPC
+    class Error < RuntimeError; end
+
     attr_reader :response
 
     def initialize(data, stream)
-      message = MessagePack.pack(data)
-      stream.write(message)
-      @response = MessagePack.unpack(stream.read)
+      @response = fetch_response(data, stream)
+    end
+
+    private
+
+    def fetch_response(data, stream)
+      stream.write(MessagePack.pack(data))
+
+      MessagePack.unpack(stream.read).tap do |payload|
+        raise Error if payload[2]
+      end
     end
   end
 end
--- spec/neovim/rpc_spec.rb
+++ spec/neovim/rpc_spec.rb
@@ -4,29 +4,30 @@ require "neovim/rpc"
 
 module Neovim
   describe RPC do
-    let(:message)  { {"message" => "Hello"} }
-    let(:response) { {"response" => "Hi"} }
+    let(:message)  { [0, 0, 1, []] }
+    let(:response) { [0, 0, nil, nil] }
 
     let(:stream) do
       double(:stream, read: MessagePack.pack(response), write: nil)
     end
 
-    describe "#initialize" do
-      it "encodes the data and writes it to the stream" do
-        packed_message = MessagePack.pack(message)
-        expect(stream).to receive(:write).with(packed_message)
-        RPC.new({message: "Hello"}, stream)
-      end
+    it "encodes the data and writes it to the stream" do
+      packed_message = MessagePack.pack(message)
+      expect(stream).to receive(:write).with(packed_message)
+      RPC.new(message, stream)
     end
 
-    describe "#response" do
-      it "reads from the stream and decodes the message" do
-        packed_response = MessagePack.pack(response)
+    it "reads from the stream and decodes the message" do
+      packed_response = MessagePack.pack(response)
+      stream.stub(:read).and_return(packed_response)
+      rpc = RPC.new(message, stream)
+      expect(rpc.response).to eq(response)
+    end
 
-        stream.stub(:read).and_return(packed_response)
-        rpc = RPC.new(message, stream)
-        expect(rpc.response).to eq(response)
-      end
+    it "raises an exception if an error is returned" do
+      error_response = [0, 0, 3, nil]
+      stream.stub(:read).and_return(MessagePack.pack(error_response))
+      expect { RPC.new(message, stream) }.to raise_error(Neovim::RPC::Error)
     end
   end
 end
