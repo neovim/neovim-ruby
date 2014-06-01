3fe1b80e1640cf4a624f84720c9ccdebf92e9469
Alex Genco <alexgenco@gmail.com>
2014-06-01 11:40:47 +0900

    use UNIXSocket instead of libuv for streams

--- lib/neovim/stream.rb
+++ lib/neovim/stream.rb
@@ -1,65 +1,33 @@
-require "libuv"
+require "socket"
 
 module Neovim
   class Stream
     class Timeout < RuntimeError; end
 
     def initialize(address, port)
-      @loop = ::Libuv::Loop.new
-      @timer = create_timer
-
-      create_uv_stream(address, port)
+      raise("TCP not supported yet") if port
+      @connection = UNIXSocket.new(address)
     end
 
     def read
-      run_until { @connected }
-      @data = nil
+      IO.select([@connection], nil, nil, 1) ||
+        raise(Timeout, "Timeout waiting for socket to be readable")
 
-      @uv.start_read
-      run_until { @data }
-      @uv.stop_read
-
-      result = @data
-      @data = nil
-      result
+      data = ""
+      loop do
+        begin
+          data << @connection.read_nonblock(4096)
+        rescue IO::WaitReadable
+          break(data)
+        end
+      end
     end
 
     def write(data)
-      run_until { @connected }
-      @written = false
+      IO.select(nil, [@connection], nil, 1) ||
+        raise(Timeout, "Timeout waiting for socket to be writable")
 
-      @uv.write(data).then do
-        @written = true
-      end
-
-      run_until { @written }
-    end
-
-    private
-
-    def create_uv_stream(address, port)
-      raise("TCP not supported yet") if port
-
-      @loop.pipe.connect(address) do |pipe|
-        pipe.progress { |data| @data = data }
-
-        @uv = pipe
-        @connected = true
-      end
-    end
-
-    def create_timer
-      @loop.timer do
-        @timeout = true
-        @loop.stop
-      end
-    end
-
-    def run_until(timeout=1000, &condition)
-      @timer.start(timeout)
-      @loop.run(:UV_RUN_ONCE) until condition.call || @timeout
-      @timer.stop
-      raise Timeout.new("Timeout of #{timeout}ms exceeded.") if @timeout
+      @connection.write(data)
     end
   end
 end
--- spec/neovim/stream_spec.rb
+++ spec/neovim/stream_spec.rb
@@ -3,16 +3,20 @@ require "msgpack"
 require "neovim/stream"
 
 module Neovim
-  describe Stream do
-    describe "using a UNIX domain socket", :remote => true do
+  describe Stream, :remote => true do
+    describe "using a UNIX domain socket" do
       it "writes data and reads responses" do
-        stream = Stream.new("/tmp/neovim.sock", nil)
+        stream = Neovim::Stream.new("/tmp/neovim.sock", nil)
         message = MessagePack.pack([0, 0, 0, []])
         stream.write(message)
-        response = MessagePack.unpack(stream.read)
 
-        expect(response).to respond_to(:to_ary)
-        expect(response.size).to eq(4)
+        response = stream.read
+        expect(response).to respond_to(:to_str)
+        expect(response).not_to be_empty
+
+        payload = MessagePack.unpack(response)
+        expect(payload).to respond_to(:to_ary)
+        expect(payload.size).to eq(4)
       end
     end
   end
