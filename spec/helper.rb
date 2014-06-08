8792250e8aee4dd897acec98b135cb5bd6084e09
Alex Genco <alexgenco@gmail.com>
2014-06-08 12:10:40 -0700

    try to connect before test suite. will fail if nvim isnt running

--- spec/helper.rb
+++ spec/helper.rb
@@ -11,20 +11,21 @@ RSpec.configure do |config|
   config.order = :random
 end
 
+# This will raise if Neovim isn't running
+Neovim::Client.new("/tmp/neovim.sock")
+
 RSpec.shared_examples :remote => true do
   let!(:client) do
-    Timeout.timeout(1) do
-      begin
-        Neovim::Client.new("/tmp/neovim.sock").command("cq")
-      rescue Errno::ENOENT, Errno::ECONNREFUSED
-        retry
-      end
+    begin
+      Neovim::Client.new("/tmp/neovim.sock").command("cq")
+    rescue Errno::ENOENT, Errno::ECONNREFUSED
+      retry
+    end
 
-      begin
-        Neovim::Client.new("/tmp/neovim.sock")
-      rescue Errno::ENOENT, Errno::ECONNREFUSED, EOFError
-        retry
-      end
+    begin
+      Neovim::Client.new("/tmp/neovim.sock")
+    rescue Errno::ENOENT, Errno::ECONNREFUSED, EOFError
+      retry
     end
   end
 end
