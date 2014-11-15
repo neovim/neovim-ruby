53d549263a7f89fd2ed9005a8490060b34c25b16
Alex Genco <alexgenco@gmail.com>
2014-11-15 13:55:30 -0800

    un-pend api compatibility spec. ignore subscribe/register commands for now

--- spec/neovim_spec.rb
+++ spec/neovim_spec.rb
@@ -2,13 +2,12 @@ require "helper"
 
 RSpec.describe Neovim, :remote => true do
   it "conforms to the API returned from Neovim" do
-    pending "Not all functions are implemented yet"
-
     lib_root = File.expand_path("../../lib", __FILE__)
     functions = @client.rpc_send(:vim_get_api_info).fetch(1).fetch("functions")
 
     functions.each do |func_data|
       func_name = func_data["name"]
+      next if func_name =~ /(subscribe|register)/
       match = %x(grep -Fr ':#{func_name}' #{lib_root})
 
       expect(match).not_to be_empty, "#{func_name} isn't implemented"
