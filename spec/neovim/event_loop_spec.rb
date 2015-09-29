71966b3fdb17e0b7ce18624f0ae2a2a3890ee5f8
Alex Genco <alexgenco@gmail.com>
2015-09-28 22:30:44 -0700

    dont write backup files in embed tests

--- spec/neovim/event_loop_spec.rb
+++ spec/neovim/event_loop_spec.rb
@@ -47,7 +47,7 @@ module Neovim
 
     context "child" do
       it "sends and receives data" do
-        event_loop = EventLoop.child(["-u", "NONE"])
+        event_loop = EventLoop.child(["-n", "-u", "NONE"])
         message = MessagePack.pack([0, 0, :vim_strwidth, ["hi"]])
 
         event_loop.send(message).run do |msg|
