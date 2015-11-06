24f51d9baa46be01cc60231dbdcc4a1ee417cec3
Alex Genco <alexgenco@gmail.com>
2015-11-06 09:11:39 -0800

    Add mocked out Host#run spec that needs a lot of love

--- spec/neovim/host_spec.rb
+++ spec/neovim/host_spec.rb
@@ -29,5 +29,52 @@ module Neovim
         expect(::Kernel.const_defined?("FooClass")).to be(false)
       end
     end
+
+    describe "#run" do
+      it "runs an async client with the plugins as callbacks" do
+        sync_cb = lambda { |nvim, x, y| [nvim, x, y] }
+        async_cb = lambda { |nvim, x, y| [nvim, x, y] }
+
+        plugin = Plugin.from_config_block do |plug|
+          plug.command(:Sync, :nargs => 2, :sync => true, &sync_cb)
+          plug.command(:Async, :nargs => 2, &async_cb)
+        end
+
+        host = Host.new([plugin])
+
+        mock_async_session = double(:async_session, :request => nil)
+        expect(AsyncSession).to receive(:new) { mock_async_session }
+
+        mock_client = double(:client)
+        expect(Client).to receive(:new) { mock_client }
+
+        expect(EventLoop).to receive(:stdio) { double(:event_loop) }
+        expect(MsgpackStream).to receive(:new) { double(:msgpack_stream) }
+        expect(Session).to receive(:new) { double(:session) }
+
+        expect(mock_async_session).to receive(:run) do |req_cb, not_cb|
+          mock_request = double(
+            :request,
+            :method_name => :Sync,
+            :arguments => [1, 2]
+          )
+
+          mock_notification = double(
+            :notification,
+            :method_name => :Async,
+            :arguments => [3, 4]
+          )
+
+          expect(sync_cb).to receive(:call).with(mock_client, 1, 2).and_call_original
+          expect(mock_request).to receive(:respond).with([mock_client, 1, 2])
+          req_cb.call(mock_request)
+
+          expect(async_cb).to receive(:call).with(mock_client, 3, 4).and_call_original
+          not_cb.call(mock_notification)
+        end
+
+        host.run
+      end
+    end
   end
 end
