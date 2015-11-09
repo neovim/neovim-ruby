4d11bcb1cc6606a910c3528116b12d25ebd1b67d
Alex Genco <alexgenco@gmail.com>
2015-11-08 20:22:34 -0800

    Acceptance test improvements
    - Run inside a tempdir
    - Remove extraneous lets and variables

--- spec/acceptance/neovim-ruby-host_spec.rb
+++ spec/acceptance/neovim-ruby-host_spec.rb
@@ -1,57 +1,51 @@
 require "helper"
-require "tempfile"
+require "tmpdir"
 
 RSpec.describe "neovim-ruby-host" do
-  let(:lib_path)  { File.expand_path("../../../lib", __FILE__) }
-  let(:bin_path)  { File.expand_path("../../../bin/neovim-ruby-host", __FILE__) }
-  let(:nvim_path) { File.expand_path("../../../vendor/neovim/build/bin/nvim", __FILE__) }
-
   specify do
-    plugin1 = Tempfile.open("plug1") do |f|
-      f.write(<<-RUBY)
-        Neovim.plugin do |plug|
-          plug.command(:SyncAdd, :args => 2, :sync => true) do |nvim, x, y|
-            x + y
+    Dir.mktmpdir do |pwd|
+      Dir.chdir(pwd) do
+        File.write("./plugin1.rb", <<-RUBY)
+          Neovim.plugin do |plug|
+            plug.command(:SyncAdd, :args => 2, :sync => true) do |nvim, x, y|
+              x + y
+            end
           end
-        end
-      RUBY
-      f.path
-    end
+        RUBY
 
-    plugin2 = Tempfile.open("plug2") do |f|
-      f.write(<<-RUBY)
-        Neovim.plugin do |plug|
-          plug.command(:AsyncSetLine, :args => 1) do |nvim, str|
-            nvim.current.line = str
+        File.write("./plugin2.rb", <<-RUBY)
+          Neovim.plugin do |plug|
+            plug.command(:AsyncSetLine, :args => 1) do |nvim, str|
+              nvim.current.line = str
+            end
           end
-        end
-      RUBY
-      f.path
+        RUBY
+
+        nvim = Neovim.attach_child(["--headless", "-u", "NONE", "-N", "-n"])
+
+        # Start the remote host
+        host_exe = File.expand_path("../../../bin/neovim-ruby-host", __FILE__)
+        nvim.command(%{let host = rpcstart("#{host_exe}", ["./plugin1.rb", "./plugin2.rb"])})
+        sleep 0.5 # TODO figure out if/why this is necessary
+
+        # Make a request to the synchronous SyncAdd method and store the results
+        nvim.command(%{let result = rpcrequest(host, "SyncAdd", 1, 2)})
+
+        # Write the results to the buffer
+        nvim.command("put =result")
+        nvim.command("normal o")
+
+        # Set the current line content via the AsyncSetLine method
+        nvim.command(%{call rpcnotify(host, "AsyncSetLine", "foo")})
+
+        # Wait for notification to be received
+        sleep 0.2
+
+        # Save the contents of the buffer
+        nvim.command("write! ./output")
+
+        expect(File.read("./output")).to eq("\n3\nfoo\n")
+      end
     end
-
-    output = Tempfile.new("output").tap(&:close).path
-    host_nvim = Neovim.attach_child(["--headless", "-u", "NONE", "-N", "-n"])
-
-    # Start the remote host
-    host_nvim.command(%{let g:chan = rpcstart("#{bin_path}", ["#{plugin1}", "#{plugin2}"])})
-    sleep 0.5 # TODO figure out if/why this is necessary
-
-    # Make a request to the synchronous SyncAdd method and store the results
-    host_nvim.command(%{let g:res = rpcrequest(g:chan, "SyncAdd", 1, 2)})
-
-    # Write the results to the buffer
-    host_nvim.command("put =g:res")
-    host_nvim.command("normal o")
-
-    # Set the current line content via the AsyncSetLine method
-    host_nvim.command(%{call rpcnotify(g:chan, "AsyncSetLine", "foo")})
-
-    # Wait for notification to be received
-    sleep 0.2
-
-    # Save the contents of the buffer
-    host_nvim.command("write! #{output}")
-
-    expect(File.read(output)).to eq("\n3\nfoo\n")
   end
 end
