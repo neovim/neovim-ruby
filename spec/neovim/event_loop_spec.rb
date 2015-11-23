5aec95e8f1ced5597e6f4dce493c1d3ee7b41c35
Alex Genco <alexgenco@gmail.com>
2015-11-22 17:56:37 -0800

    Add crazy test around EventLoop.stdio

--- spec/neovim/event_loop_spec.rb
+++ spec/neovim/event_loop_spec.rb
@@ -44,6 +44,42 @@ module Neovim
       include_context "socket behavior"
     end
 
+    context "stdio" do
+      it "sends and receives data" do
+        old_stdout = STDOUT.dup
+        old_stdin = STDIN.dup
+
+        begin
+          srv_stdout, cl_stdout = IO.pipe
+          cl_stdin, srv_stdin = IO.pipe
+
+          STDOUT.reopen(cl_stdout)
+          STDIN.reopen(cl_stdin)
+
+          event_loop = EventLoop.stdio
+          messages = []
+
+          srv_thr = Thread.new do
+            messages << srv_stdout.readpartial(1024)
+            srv_stdin.write("OK")
+          end
+
+          fiber = Fiber.new do
+            event_loop.send("data").run do |msg|
+              Fiber.yield(msg)
+            end
+          end
+
+          expect(fiber.resume).to eq("OK")
+          srv_thr.join
+          expect(messages).to eq(["data"])
+        ensure
+          STDOUT.reopen(old_stdout)
+          STDIN.reopen(old_stdin)
+        end
+      end
+    end
+
     context "child" do
       it "sends and receives data" do
         event_loop = EventLoop.child(["-n", "-u", "NONE"])
