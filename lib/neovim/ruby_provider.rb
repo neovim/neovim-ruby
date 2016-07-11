0ebf2412e3e9240788b7576cff9b1b7dcb08e8a8
Alex Genco <alexgenco@gmail.com>
2016-07-10 20:30:13 -0700

    More ruby_provider cleanup
    - "Obfuscate" every method to limit exposure in rubydo handling
    - Add documentation
    - Set Thread.abort_on_exception in `__define_plugin!`

--- lib/neovim/ruby_provider.rb
+++ lib/neovim/ruby_provider.rb
@@ -1,38 +1,57 @@
-Thread.abort_on_exception = true
-
 require "neovim/ruby_provider/vim"
 require "neovim/ruby_provider/buffer_ext"
 require "neovim/ruby_provider/window_ext"
 
 module Neovim
+  # This class is used to define a +Neovim::Plugin+ to act as a backend for the
+  # legacy +:ruby+, +:rubyfile+, and +:rubydo+ Vim commands. It is autoloaded
+  # from +nvim+ and not intended to be loaded directly.
   module RubyProvider
-    def self.define_plugin!
+    def self.__define_plugin!
+      Thread.abort_on_exception = true
+
       Neovim.plugin do |plug|
-        define_ruby_execute(plug)
-        define_ruby_execute_file(plug)
-        define_ruby_do_range(plug)
+        __define_ruby_execute(plug)
+        __define_ruby_execute_file(plug)
+        __define_ruby_do_range(plug)
       end
     end
 
-    def self.define_ruby_execute(plug)
+    # Evaluate the provided Ruby code, exposing the +VIM+ constant for
+    # interactions with the editor.
+    #
+    # This is used by the +:ruby+ command.
+    def self.__define_ruby_execute(plug)
       plug.rpc(:ruby_execute, sync: true) do |nvim, ruby|
-        wrap_client(nvim) do
+        __wrap_client(nvim) do
           eval(ruby, TOPLEVEL_BINDING, __FILE__, __LINE__)
         end
       end
     end
-    private_class_method :define_ruby_execute
+    private_class_method :__define_ruby_execute
 
-    def self.define_ruby_execute_file(plug)
+    # Evaluate the provided Ruby file, exposing the +VIM+ constant for
+    # interactions with the editor.
+    #
+    # This is used by the +:rubyfile+ command.
+    def self.__define_ruby_execute_file(plug)
       plug.rpc(:ruby_execute_file, sync: true) do |nvim, path|
-        wrap_client(nvim) { load(path) }
+        __wrap_client(nvim) { load(path) }
       end
     end
-    private_class_method :define_ruby_execute_file
+    private_class_method :__define_ruby_execute_file
 
-    def self.define_ruby_do_range(__plug)
+    # Evaluate the provided Ruby code over each line of a range. The contents
+    # of the current line can be accessed and modified via the +$_+ variable.
+    #
+    # Since this method evaluates each line in the local binding, all local
+    # variables and methods are available to the user. Thus the +__+ prefix
+    # obfuscation.
+    #
+    # This is used by the +:rubydo+ command.
+    def self.__define_ruby_do_range(__plug)
       __plug.rpc(:ruby_do_range, sync: true) do |__nvim, *__args|
-        wrap_client(__nvim) do
+        __wrap_client(__nvim) do
           __start, __stop, __ruby = __args
           __buffer = __nvim.get_current_buffer
 
@@ -46,38 +65,39 @@ module Neovim
         end
       end
     end
-    private_class_method :define_ruby_do_range
+    private_class_method :__define_ruby_do_range
 
-    def self.wrap_client(__client)
-      with_globals(__client) do
-        with_vim_constant(__client) do
-          with_redirect_streams(__client) do
+    # @api private
+    def self.__wrap_client(client)
+      __with_globals(client) do
+        __with_vim_constant(client) do
+          __with_redirect_streams(client) do
             yield
           end
         end
       end
       nil
     end
-    private_class_method :wrap_client
+    private_class_method :__wrap_client
 
-    def self.with_globals(client)
+    # @api private
+    def self.__with_globals(client)
       $curbuf = client.get_current_buffer
       $curwin = client.get_current_window
       yield
     end
-    private_class_method :with_globals
+    private_class_method :__with_globals
 
-    def self.with_vim_constant(client)
+    # @api private
+    def self.__with_vim_constant(client)
       ::VIM.__client = client
       yield
     end
-    private_class_method :with_vim_constant
+    private_class_method :__with_vim_constant
 
-    def self.with_redirect_streams(client)
-      @with_redirect_streams ||= begin
-        old_out_write = $stdout.method(:write)
-        old_err_write = $stderr.method(:write)
-
+    # @api private
+    def self.__with_redirect_streams(client)
+      @__with_redirect_streams ||= begin
         $stdout.define_singleton_method(:write) do |string|
           client.out_write(string)
         end
@@ -91,8 +111,9 @@ module Neovim
 
       yield
     end
-    private_class_method :with_redirect_streams
+    private_class_method :__with_redirect_streams
 
+    # @api private
     def self.__update_lines_in_chunks(buffer, start, stop, size)
       (start..stop).each_slice(size) do |linenos|
         _start, _stop = linenos[0]-1, linenos[-1]
@@ -105,4 +126,4 @@ module Neovim
   end
 end
 
-Neovim::RubyProvider.define_plugin!
+Neovim::RubyProvider.__define_plugin!
