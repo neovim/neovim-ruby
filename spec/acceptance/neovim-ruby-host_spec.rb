aa80d9135fd71cafc216976072c9f555e69bf976
Alex Genco <alexgenco@gmail.com>
2016-07-26 18:33:04 -0700

    Acceptance test improvements
    - Add test around --version and -V flags
    - Add test around running non-interactively
    - Simplify test around loading plugins

--- spec/acceptance/neovim-ruby-host_spec.rb
+++ spec/acceptance/neovim-ruby-host_spec.rb
@@ -1,25 +1,47 @@
 require "helper"
+require "pty"
 
 RSpec.describe "neovim-ruby-host" do
+  let(:host_exe) do
+    File.expand_path("../../../bin/neovim-ruby-host", __FILE__)
+  end
+
+  it "prints the gem version" do
+    ["--version", "-V"].each do |opt|
+      expect {
+        system(host_exe, opt)
+      }.to output("#{Neovim::VERSION}\n").to_stdout_from_any_process
+    end
+  end
+
+  it "fails when attached to a TTY" do
+    yielded = false
+
+    PTY.spawn(host_exe) do |rd, wr, pid|
+      yielded = true
+      expect(rd.gets).to match(/can't run.+interactively/i)
+
+      _, status = Process.waitpid2(pid)
+      expect(status.exitstatus).to be(1)
+    end
+
+    expect(yielded).to be(true)
+  end
+
   it "loads and runs plugins from Ruby source files" do
-    plugin1_path = Support.file_path("plugin1.rb")
-    File.write(plugin1_path, <<-RUBY)
+    plugin_path = Support.file_path("plugin1.rb")
+    File.write(plugin_path, <<-RUBY)
       Neovim.plugin do |plug|
+        plug.command(:AsyncSetLine, :args => 1) do |nvim, str|
+          nvim.current.line = str
+        end
+
         plug.function(:SyncAdd, :args => 2, :sync => true) do |nvim, x, y|
           x + y
         end
 
         plug.autocmd(:BufEnter, :pattern => "*.rb") do |nvim|
           nvim.current.line = "Ruby file, eh?"
-        end
-      end
-    RUBY
-
-    plugin2_path = Support.file_path("plugin2.rb")
-    File.write(plugin2_path, <<-RUBY)
-      Neovim.plugin do |plug|
-        plug.command(:AsyncSetLine, :args => 1) do |nvim, str|
-          nvim.current.line = str
         end
 
         plug.rpc(:TopLevelAdd, :nargs => 2, :sync => true) do |nvim, x, y|
@@ -30,21 +52,18 @@ RSpec.describe "neovim-ruby-host" do
 
     nvim = Neovim.attach_child(["nvim", "-u", "NONE", "-n"])
 
-    host_exe = File.expand_path("../../../bin/neovim-ruby-host", __FILE__)
-    nvim.command("let host = rpcstart('#{host_exe}', ['#{plugin1_path}', '#{plugin2_path}'])")
+    nvim.command("let host = rpcstart('#{host_exe}', ['#{plugin_path}'])")
 
     expect(nvim.eval("rpcrequest(host, 'poll')")).to eq("ok")
-    expect(nvim.eval("rpcrequest(host, '#{plugin1_path}:function:SyncAdd', [1, 2])")).to eq(3)
+    expect(nvim.eval("rpcrequest(host, '#{plugin_path}:function:SyncAdd', [1, 2])")).to eq(3)
     expect(nvim.eval("rpcrequest(host, 'TopLevelAdd', 1, 2)")).to eq(3)
 
     expect {
-      nvim.command("call rpcnotify(host, '#{plugin1_path}:autocmd:BufEnter:*.rb')")
-      sleep 0.1
+      nvim.command("call rpcnotify(host, '#{plugin_path}:autocmd:BufEnter:*.rb')")
     }.to change { nvim.current.buffer.lines.to_a }.from([""]).to(["Ruby file, eh?"])
 
     expect {
-      nvim.command("call rpcnotify(host, '#{plugin2_path}:command:AsyncSetLine', ['foo'])")
-      sleep 0.1
+      nvim.command("call rpcnotify(host, '#{plugin_path}:command:AsyncSetLine', ['foo'])")
     }.to change { nvim.current.buffer.lines.to_a }.from(["Ruby file, eh?"]).to(["foo"])
 
     expect {
