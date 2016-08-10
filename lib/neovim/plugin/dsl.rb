a0ea0e8a2ac11f237f7009455e4c628a9a5a7be1
Alex Genco <alexgenco@gmail.com>
2016-08-09 18:55:51 -0700

    Improve Plugin DSL docs

--- lib/neovim/plugin/dsl.rb
+++ lib/neovim/plugin/dsl.rb
@@ -3,51 +3,65 @@ require "neovim/plugin/handler"
 module Neovim
   class Plugin
     # The DSL exposed in +Neovim.plugin+ blocks.
+    #
+    # @api public
     class DSL < BasicObject
       def initialize(plugin)
         @plugin = plugin
       end
 
-      # Register an +nvim+ command.
+      # Register an +nvim+ command. See +:h command+.
       #
-      # @param name [String]
-      # @param options [Hash]
-      # @param &block [Proc, nil]
+      # @param name [String] The name of the command.
+      # @param options [Hash] Command options.
+      # @param &block [Proc, nil] The body of the command.
       #
-      # @option options [Fixnum] :nargs
-      # @option options [Fixnum] :count
-      # @option options [String] :eval
-      # @option options [Boolean] :sync (false)
-      # @option options [Boolean] :bang
-      # @option options [Boolean] :register
-      # @option options [Boolean] :complete
-      # @option options [String, Boolean] :range
+      # @option options [Fixnum] :nargs The number of arguments to accept. See
+      #   +:h command-nargs+.
+      # @option options [String, Boolean] :range The range argument.
+      #   See +:h command-range+.
+      # @option options [Fixnum] :count The default count argument.
+      #   See +:h command-count+.
+      # @option options [Boolean] :bang Whether the command can take a +!+
+      #   modifier. See +:h command-bang+.
+      # @option options [Boolean] :register Whether the command can accept a
+      #   register name. See +:h command-register+.
+      # @option options [Boolean] :complete Set the completion attributes of
+      #   the command. See +:h command-completion+.
+      # @option options [String] :eval An +nvim+ expression. Gets evaluated and
+      #   passed as an argument to the block.
+      # @option options [Boolean] :sync (false) Whether +nvim+ should receive
+      #   the return value of the block.
       def command(name, options={}, &block)
         register_handler(:command, name, options, block)
       end
 
-      # Register an +nvim+ function.
+      # Register an +nvim+ function. See +:h function+.
       #
-      # @param name [String]
-      # @param options [Hash]
-      # @param &block [Proc, nil]
+      # @param name [String] The name of the function.
+      # @param options [Hash] Function options.
+      # @param &block [Proc, nil] The body of the function.
       #
-      # @option options [String] :eval
-      # @option options [Boolean] :sync (false)
-      # @option options [String, Boolean] :range
+      # @option options [String, Boolean] :range The range argument. 
+      #   See +:h command-range+.
+      # @option options [String] :eval An +nvim+ expression. Gets evaluated and
+      #   passed as an argument to the block.
+      # @option options [Boolean] :sync (false) Whether +nvim+ should receive
+      #   the return value of the block.
       def function(name, options={}, &block)
         register_handler(:function, name, options, block)
       end
 
-      # Register an +nvim+ autocmd.
+      # Register an +nvim+ autocmd. See +:h autocmd+.
       #
-      # @param event [String]
-      # @param options [Hash]
-      # @param &block [Proc, nil]
+      # @param event [String] The event type. See +:h autocmd-events+
+      # @param options [Hash] Autocmd options.
+      # @param &block [Proc, nil] The body of the autocmd.
       #
-      # @option options [String] :pattern
-      # @option options [String] :eval
-      # @option options [Boolean] :sync (false)
+      # @option options [String] :pattern The buffer name pattern.
+      #   See +:h autocmd-patterns+.
+      # @option options [String] :eval An +nvim+ expression. Gets evaluated and
+      #   passed as an argument to the block.
       def autocmd(event, options={}, &block)
         register_handler(:autocmd, event, options, block)
       end
@@ -57,7 +71,8 @@ module Neovim
       # This can be used to directly expose an RPC call without a namespace.
       # This is used primarily for exposing legacy ruby provider calls.
       #
-      # @option options [Boolean] :sync (false)
+      # @option options [Boolean] :sync (false) Whether +nvim+ should receive
+      #   the return value of the block.
       def rpc(name, options={}, &block)
         sync = options.delete(:sync)
 
