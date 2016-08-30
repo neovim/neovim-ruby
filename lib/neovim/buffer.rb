fdc06b5c64aab887f74c4c94d3935c253fa304d3
Alex Genco <alexgenco@gmail.com>
2016-08-30 10:41:08 -0700

    Add doc string to generated docs

--- lib/neovim/buffer.rb
+++ lib/neovim/buffer.rb
@@ -151,22 +151,27 @@ module Neovim
 # The following methods are dynamically generated.
 =begin
 @method line_count
+  Send the +line_count+ RPC to +nvim+
   @return [Fixnum]
 
 @method get_line(index)
+  Send the +get_line+ RPC to +nvim+
   @param [Fixnum] index
   @return [String]
 
 @method set_line(index, line)
+  Send the +set_line+ RPC to +nvim+
   @param [Fixnum] index
   @param [String] line
   @return [void]
 
 @method del_line(index)
+  Send the +del_line+ RPC to +nvim+
   @param [Fixnum] index
   @return [void]
 
 @method get_line_slice(start, end, include_start, include_end)
+  Send the +get_line_slice+ RPC to +nvim+
   @param [Fixnum] start
   @param [Fixnum] end
   @param [Boolean] include_start
@@ -174,12 +179,14 @@ module Neovim
   @return [Array<String>]
 
 @method get_lines(start, end, strict_indexing)
+  Send the +get_lines+ RPC to +nvim+
   @param [Fixnum] start
   @param [Fixnum] end
   @param [Boolean] strict_indexing
   @return [Array<String>]
 
 @method set_line_slice(start, end, include_start, include_end, replacement)
+  Send the +set_line_slice+ RPC to +nvim+
   @param [Fixnum] start
   @param [Fixnum] end
   @param [Boolean] include_start
@@ -188,6 +195,7 @@ module Neovim
   @return [void]
 
 @method set_lines(start, end, strict_indexing, replacement)
+  Send the +set_lines+ RPC to +nvim+
   @param [Fixnum] start
   @param [Fixnum] end
   @param [Boolean] strict_indexing
@@ -195,50 +203,62 @@ module Neovim
   @return [void]
 
 @method get_var(name)
+  Send the +get_var+ RPC to +nvim+
   @param [String] name
   @return [Object]
 
 @method set_var(name, value)
+  Send the +set_var+ RPC to +nvim+
   @param [String] name
   @param [Object] value
   @return [Object]
 
 @method del_var(name)
+  Send the +del_var+ RPC to +nvim+
   @param [String] name
   @return [Object]
 
 @method get_option(name)
+  Send the +get_option+ RPC to +nvim+
   @param [String] name
   @return [Object]
 
 @method set_option(name, value)
+  Send the +set_option+ RPC to +nvim+
   @param [String] name
   @param [Object] value
   @return [void]
 
 @method get_number
+  Send the +get_number+ RPC to +nvim+
   @return [Fixnum]
 
 @method get_name
+  Send the +get_name+ RPC to +nvim+
   @return [String]
 
 @method set_name(name)
+  Send the +set_name+ RPC to +nvim+
   @param [String] name
   @return [void]
 
 @method is_valid
+  Send the +is_valid+ RPC to +nvim+
   @return [Boolean]
 
 @method insert(lnum, lines)
+  Send the +insert+ RPC to +nvim+
   @param [Fixnum] lnum
   @param [Array<String>] lines
   @return [void]
 
 @method get_mark(name)
+  Send the +get_mark+ RPC to +nvim+
   @param [String] name
   @return [Array<Fixnum>]
 
 @method add_highlight(src_id, hl_group, line, col_start, col_end)
+  Send the +add_highlight+ RPC to +nvim+
   @param [Fixnum] src_id
   @param [String] hl_group
   @param [Fixnum] line
@@ -247,6 +267,7 @@ module Neovim
   @return [Fixnum]
 
 @method clear_highlight(src_id, line_start, line_end)
+  Send the +clear_highlight+ RPC to +nvim+
   @param [Fixnum] src_id
   @param [Fixnum] line_start
   @param [Fixnum] line_end
--- lib/neovim/window.rb
+++ lib/neovim/window.rb
@@ -59,58 +59,73 @@ module Neovim
 # The following methods are dynamically generated.
 =begin
 @method get_buffer
+  Send the +get_buffer+ RPC to +nvim+
   @return [Buffer]
 
 @method get_cursor
+  Send the +get_cursor+ RPC to +nvim+
   @return [Array<Fixnum>]
 
 @method set_cursor(pos)
+  Send the +set_cursor+ RPC to +nvim+
   @param [Array<Fixnum>] pos
   @return [void]
 
 @method get_height
+  Send the +get_height+ RPC to +nvim+
   @return [Fixnum]
 
 @method set_height(height)
+  Send the +set_height+ RPC to +nvim+
   @param [Fixnum] height
   @return [void]
 
 @method get_width
+  Send the +get_width+ RPC to +nvim+
   @return [Fixnum]
 
 @method set_width(width)
+  Send the +set_width+ RPC to +nvim+
   @param [Fixnum] width
   @return [void]
 
 @method get_var(name)
+  Send the +get_var+ RPC to +nvim+
   @param [String] name
   @return [Object]
 
 @method set_var(name, value)
+  Send the +set_var+ RPC to +nvim+
   @param [String] name
   @param [Object] value
   @return [Object]
 
 @method del_var(name)
+  Send the +del_var+ RPC to +nvim+
   @param [String] name
   @return [Object]
 
 @method get_option(name)
+  Send the +get_option+ RPC to +nvim+
   @param [String] name
   @return [Object]
 
 @method set_option(name, value)
+  Send the +set_option+ RPC to +nvim+
   @param [String] name
   @param [Object] value
   @return [void]
 
 @method get_position
+  Send the +get_position+ RPC to +nvim+
   @return [Array<Fixnum>]
 
 @method get_tabpage
+  Send the +get_tabpage+ RPC to +nvim+
   @return [Tabpage]
 
 @method is_valid
+  Send the +is_valid+ RPC to +nvim+
   @return [Boolean]
 
 =end
