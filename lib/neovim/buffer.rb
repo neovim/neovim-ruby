39cc8cf6718b38455262d484f5725a9bbb3c9ddd
Docs Workflow <noreply@github.com>
2020-08-09 00:40:08 +0000

    [skip ci] Update generated docs

--- lib/neovim/buffer.rb
+++ lib/neovim/buffer.rb
@@ -4,7 +4,7 @@ require "neovim/line_range"
 module Neovim
   # Class representing an +nvim+ buffer.
   #
-  # The methods documented here were generated using NVIM v0.4.3
+  # The methods documented here were generated using NVIM v0.4.4
   class Buffer < RemoteObject
     attr_reader :lines
 
