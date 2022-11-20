f5a609d9a4ab6f279dd5b730b4bb46bde723690d
Docs Workflow <noreply@github.com>
2022-11-20 01:58:46 +0000

    [skip ci] Update generated docs

--- lib/neovim/client.rb
+++ lib/neovim/client.rb
@@ -9,7 +9,7 @@ module Neovim
   # +RemoteObject+ subclasses (i.e. +Buffer+, +Window+, or +Tabpage+),
   # which similarly have dynamically generated interfaces.
   #
-  # The methods documented here were generated using NVIM v0.8.0-1210-gd367ed9b2
+  # The methods documented here were generated using NVIM v0.8.1
   #
   # @see Buffer
   # @see Window
