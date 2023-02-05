0e5593b197ffd70be960f83213c69b0d30ecde1f
Docs Workflow <noreply@github.com>
2023-02-05 01:43:08 +0000

    [skip ci] Update generated docs

--- lib/neovim/window.rb
+++ lib/neovim/window.rb
@@ -3,7 +3,7 @@ require "neovim/remote_object"
 module Neovim
   # Class representing an +nvim+ window.
   #
-  # The methods documented here were generated using NVIM v0.8.2
+  # The methods documented here were generated using NVIM v0.8.3
   class Window < RemoteObject
     # Get the buffer displayed in the window
     #
